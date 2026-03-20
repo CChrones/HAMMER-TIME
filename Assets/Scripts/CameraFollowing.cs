b566e0922e37becccc9f265c5216e1b7e2ce1d74
CChrones <cchrones@mail.bradley.edu>
2026-03-20 01:52:37 -0500

    .
    .

--- Assets/Scripts/CameraFollowing.cs
+++ Assets/Scripts/CameraFollowing.cs
@@ -1,16 +1,83 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class CameraFollowing : MonoBehaviour
 {
+    private InputAction moveR;
+    private InputAction moveL;
+    private Rigidbody rb;
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private Transform playerObject; 
+    [SerializeField] private PlayerMovement PM; 
+    private bool canMoveCamera;
+
+    private int movingWayCamera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        moveR = InputSystem.actions.FindAction("MoveR");
+        moveL = InputSystem.actions.FindAction("MoveL");
+
+        moveR.performed += MoveRPerformed;
+        moveR.canceled += MoveRCanceled;
+
+        moveL.performed += MoveLPerformed;
+        moveL.canceled += MoveLCanceled;
+        canMoveCamera = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(canMoveCamera){
+            if(movingWayCamera == 1){
+                transform.RotateAround(playerObject.position, Vector3.up, cameraSpeed * Time.deltaTime);
+            }
+            if(movingWayCamera == 2){
+                transform.RotateAround(playerObject.position, -Vector3.up, cameraSpeed * Time.deltaTime);
+            }
+        }
+        if(PM.canMove){
+            if(PM.movingWay == 1){
+                transform.Translate(transform.forward * Time.deltaTime * PM.playerSpeed);
+            }
+            if(PM.movingWay == 2){
+                transform.Translate(-transform.forward * Time.deltaTime * PM.playerSpeed);
+            }
+        }
     }
+    private void MoveRPerformed(InputAction.CallbackContext obj)
+    {
+        movingWayCamera = 1;
+    }
+
+    private void MoveRCanceled(InputAction.CallbackContext obj)
+    {
+        if(movingWayCamera == 1){
+        movingWayCamera = 0;
+        }
+    }
+
+    private void MoveLPerformed(InputAction.CallbackContext obj)
+    {
+        movingWayCamera = 2;
+    }
+
+    private void MoveLCanceled(InputAction.CallbackContext obj)
+    {
+        if(movingWayCamera == 2){
+        movingWayCamera = 0;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        moveL.performed -= MoveLPerformed;
+        moveL.canceled -= MoveLCanceled;
+
+        moveR.performed -= MoveRPerformed;
+        moveR.canceled -= MoveRCanceled;
+    }
+    
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,79 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
+    private InputAction moveF;
+    private InputAction moveB;
+    private Vector3 playerMovement;
+    private Rigidbody rb;
+    [SerializeField] public float playerSpeed;
+    [SerializeField] private Transform cameraTransform; 
+
+    public bool canMove;
+
+    public int movingWay;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (cameraTransform == null)
+        {
+            //Finds the main camera if not assigned in the Inspector
+            cameraTransform = Camera.main.transform; 
+        }
+        rb = GetComponent<Rigidbody>();
+
+        moveF = InputSystem.actions.FindAction("MoveF");
+        moveB = InputSystem.actions.FindAction("MoveB");
+
+        moveF.performed += MoveFPerformed;
+        moveF.canceled += MoveFCanceled;
+
+        moveB.performed += MoveBPerformed;
+        moveB.canceled += MoveBCanceled;
+        canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(canMove){
+            if(movingWay == 1){
+                transform.Translate(cameraTransform.forward * Time.deltaTime * playerSpeed);
+            }
+            if(movingWay == 2){
+                transform.Translate(-cameraTransform.forward * Time.deltaTime * playerSpeed);
+            }
+        }
+    }
+    private void MoveFPerformed(InputAction.CallbackContext obj)
+    {
+        movingWay = 1;
+    }
+
+    private void MoveFCanceled(InputAction.CallbackContext obj)
+    {
+        if(movingWay == 1){
+        movingWay = 0;
+        }
+    }
+
+    private void MoveBPerformed(InputAction.CallbackContext obj)
+    {
+        movingWay = 2;
+    }
+
+    private void MoveBCanceled(InputAction.CallbackContext obj)
+    {
+        if(movingWay == 2){
+        movingWay = 0;
+        }
+    }
+    private void OnDestroy()
+    {
+        moveF.performed -= MoveFPerformed;
+        moveF.canceled -= MoveFCanceled;
+
+        moveB.performed -= MoveBPerformed;
+        moveB.canceled -= MoveBCanceled;
     }
 }
