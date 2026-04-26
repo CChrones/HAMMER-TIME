3e51369133634d936bbf76096eaf72a186465a83
CChrones <cchrones@mail.bradley.edu>
2026-04-26 00:29:35 -0500

    .
    .

--- Assets/Scripts/QuestPoints.cs
+++ Assets/Scripts/QuestPoints.cs
@@ -6,6 +6,7 @@
 //                     quest system for the player
 //****************************************************************************
 using UnityEngine;
+using TMPro;
 
 public class QuestPoints : MonoBehaviour
 {
@@ -14,18 +15,31 @@ public class QuestPoints : MonoBehaviour
     [SerializeField] private GameObject questReward;
     [SerializeField] private Vector3 transformOffset;
     [SerializeField] private int gameObjectAmount;
+    [SerializeField] private TMP_Text questText;
+    [SerializeField] private string QT1;
+    [SerializeField] private string QT2;
+    private bool hasRewarded;
+
+    void Start(){
+        hasRewarded = false;
+    }
 
 //quest system
     void Update()
     {
         //if you have enough quest points, summon thing.
-        if(questPoints >= questPointsRequired)
+        if(questPoints >= questPointsRequired && hasRewarded == false)
         {
-            questPoints = 0;
+            hasRewarded = true;
             for(int i = 0; i < gameObjectAmount; i++){
             Instantiate(questReward, new Vector3(transform.position.x + transformOffset.x, transform.position.y + transformOffset.y,
                 transform.position.z + transformOffset.z), Quaternion.identity);
             }
+        }
+        if(questPointsRequired > 1){
+            questText.text = QT1 + " " + questPoints + "/" + questPointsRequired + " " + QT2;
+        } else {
+            questText.text = QT1 + " " + QT2;
         }
     }
 }
